caec921955af9ed58bafeb0375ae2ef6ff5495af
Santiago Ceron <ceronsanti@gmail.com>
2023-09-02 11:09:01 -0300

    Changing Back-end to Paradigm project

--- Back-end/src/config/db.ts
+++
@@ -1,7 +0,0 @@
-export const dbconfig = {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_DB,
-    port: Number(process.env.DB_PORT),
-}
--- Back-end/src/routes/events.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getAllEvents } from "../controllers/events";
-
-const router = Router();
-
-router.get("/events", getAllEvents);
-
-export default router;
