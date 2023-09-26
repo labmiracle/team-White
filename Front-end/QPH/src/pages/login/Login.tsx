2fe00a3af008967c4decb44b2d10b9f5915f8a2f
jeune07 <jeunewinsley9@gmail.com>
2023-09-26 01:32:12 -0300

    loggin front

--- Front-end/QPH/src/pages/login/Login.tsx
+++ Front-end/QPH/src/pages/login/Login.tsx
@@ -1,7 +1,35 @@
 import style from "./login.module.css"
 import loginconcertimg from "../../assets/Concert-login.png"
 import Nav from "../../commonComponents/nav/Nav"
+import { useState } from "react"
+import axios from "axios"
+
+
+
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        
+        try {
+            const response = await axios.post('http://localhost:5000/Auth/AuthLogin', {
+              email,
+              password,
+            });
+          
+            console.log('Response:', response.data);
+            // Assuming your server responds with a JWT token.
+            // const { token } = response.data;
+            window.location.href = '/main';
+          } catch (error) {
+            console.error('Login failed:', error);
+            setError('Algo anda mal por favor revisa el e-email o la contraseña');
+          }
+    };
+
   return (
     
     <div className={""}>
@@ -10,9 +38,11 @@ const Login = () => {
         <div className={style.formCOntainner}> 
             <form className={`${style.formContainer} form`} action="" method="post">
                 <p>Ingresar</p>
-                <input type="text" placeholder="tucorreo@gmail.com"  name="emai" id="emai"/>
-                <input type="text" placeholder="password" name="password" id="password"/>
-                <input type="button" value="Ingresar" id="login" className={style.loginbtn}/>
+                <input type="text" placeholder="tucorreo@gmail.com"  name="emaiL" id="email" value={email} onChange={(e)=> setEmail(e.target.value)}/>
+                <input type="password" placeholder="password" name="password" id="password" value={password} onChange={(e)=> setPassword(e.target.value)}/>
+                <input type="submit" value="Ingresar" id="login" className={style.loginbtn} onClick={handleSubmit}/>
+                {error && <p className={style.errorMessage}>{error}</p>}
+               
             </form>
         </div>
         <div className="">
--- Front-end/QPH/src/pages/login/Register.tsx
+++ Front-end/QPH/src/pages/login/Register.tsx
@@ -1,40 +1,70 @@
 import style from "./login.module.css"
 import loginconcertimg from "../../assets/Concert-login.png"
 import Nav from "../../commonComponents/nav/Nav"
+import { useState } from "react"
+import axios from "axios"
+
 const Register = () => {
+  const [name, setName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [active, setActive] = useState(false);
+  const [artist, setArtist] = useState(false);
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post("http://localhost:5000/Auth/AuthRegister", {
+        name: name,
+        lastName: lastName,
+        email: email,
+        password: password,
+        userType: artist ? "artist" : "organization",
+        active: active ? 1 : 0
+      });
+
+      // Handle the response as needed
+      console.log("Registration Successful:", response.data);
+    } catch (error) {
+      console.error("Error during registration:", error);
+    }
+  }
+
   return (
-    
-    <div className={""}>
-    <Nav/>
-    <div className={style.loginContainer}>    
-        <div className={style.formCOntainner}> 
-            <form className={`${style.formContainer} form`} action="" method="post">
-                <p>Ingresar</p>
-                <input type="text" placeholder="tucorreo@gmail.com"  name="emai" id="emai"/>
-                <div className={style.nameContainer}>
-                <input type="text" placeholder="Tu nombre"  name="name" id="name"/>
-                <input type="text" placeholder="Tu apellido"  name="lastName" id="lastName"/>
-                </div>
-                <input type="text" placeholder="password" name="password" id="password"/>
-                <div className="">
-                    <span>Soy una una organizacíon</span>
-                    <input type="checkbox" name="organizacion" id="" />
-                </div>
-                <div>
-                <input type="button" value="Crear Cuenta" id="login" className={style.loginbtn}/>
-                <span>Conectar</span>
-                </div>
-            </form>
+    <div className="">
+      <Nav />
+      <div className={style.loginContainer}>
+        <div className={style.formContainer}>
+          <form className={`${style.formContainer} form`} onSubmit={handleRegister}>
+            <p>Ingresar</p>
+            <input type="text" placeholder="tucorreo@gmail.com" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <div className={style.nameContainer}>
+              <input type="text" placeholder="Tu nombre" name="name" id="name" value={name} onChange={(e) => setName(e.target.value)} />
+              <input type="text" placeholder="Tu apellido" name="lastName" id="lastName" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+            </div>
+            <input type="text" placeholder="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <div className="">
+              <span>Soy una organización</span>
+              <input type="checkbox" name="organizacion" id="organizacion" checked={active} onChange={(e) => setActive(e.target.checked)} />
+              <span>Soy un artista</span>
+              <input type="checkbox" name="artist" id="artist" checked={artist} onChange={(e) => setArtist(e.target.checked)} />
+            </div>
+            <div>
+              <input type="submit" value="Crear Cuenta" id="login" className={style.loginbtn} />
+              <span>Conectar</span>
+            </div>
+          </form>
         </div>
         <div className="">
-            <figure>
-                <img src={loginconcertimg} alt="imagen de un concierto" />
-            </figure>
+          <figure>
+            <img src={loginconcertimg} alt="imagen de un concierto" />
+          </figure>
         </div>
-    </div>
+      </div>
     </div>
   )
- 
 }
 
 export default Register;
