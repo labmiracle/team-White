aa9ec8cc218a1439a63a877a38b2949c5eb51a21
jeune07 <jeunewinsley9@gmail.com>
2023-09-28 18:10:39 -0300

    login and register actualized

--- Front-end/QPH/src/pages/login/Register.tsx
+++ Front-end/QPH/src/pages/login/Register.tsx
@@ -5,24 +5,26 @@ import { useState } from "react";
 import axios from "axios";
 
 const Register = () => {
+
   const [name, setName] = useState('');
   const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
+  const [mail, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [active, setActive] = useState(false);
-  const [artist, setArtist] = useState(false);
+  const [alias, setAlias] = useState("");
+  const [userType, setUserType] = useState(false);
 
   const handleRegister = async (e: { preventDefault: () => void; }) => {
     e.preventDefault();
 
     try {
-      const response = await axios.post("http://localhost:5000/Auth/AuthRegister", {
+      const response = await axios.post("http://localhost:5000/api/auth/register", {
         name: name,
         lastName: lastName,
-        email: email,
+        alias: alias,
+        mail: mail,
         password: password,
-        userType: artist ? "artist" : "organization",
-        active: active ? 1 : 0
+        userType: userType,
+       
       });
 
       // Handle the response as needed
@@ -31,7 +33,7 @@ const Register = () => {
       console.error("Error during registration:", error);
     }
   }
-
+ console.log(handleRegister)
   return (
     <div className="">
       <Nav />
@@ -39,17 +41,18 @@ const Register = () => {
         <div className={style.formContainer}>
           <form className={`${style.formContainer} form`} onSubmit={handleRegister}>
             <p>Ingresar</p>
-            <input type="text" placeholder="tucorreo@gmail.com" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <input type="text" placeholder="tucorreo@gmail.com" name="email" id="email" value={mail} onChange={(e) => setEmail(e.target.value)} />
             <div className={style.nameContainer}>
               <input type="text" placeholder="Tu nombre" name="name" id="name" value={name} onChange={(e) => setName(e.target.value)} />
               <input type="text" placeholder="Tu apellido" name="lastName" id="lastName" value={lastName} onChange={(e) => setLastName(e.target.value)} />
             </div>
             <input type="text" placeholder="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <input type="text" placeholder="alias" name="alias" id="alias" value={alias} onChange={(e) => setAlias(e.target.value)} />
             <div className="">
               <span>Soy una organización</span>
-              <input type="checkbox" name="organizacion" id="organizacion" checked={active} onChange={(e) => setActive(e.target.checked)} />
+              {/*<input type="checkbox" name="organizacion" id="organizacion" checked={active} onChange={(e) => setActive(e.target.checked)} />*/}
               <span>Soy un artista</span>
-              <input type="checkbox" name="artist" id="artist" checked={artist} onChange={(e) => setArtist(e.target.checked)} />
+              <input type="checkbox" name="userType" id="artist" checked={userType} onChange={(e) => setUserType(e.target.checked)} />
             </div>
             <div>
               <input type="submit" value="Crear Cuenta" id="login" className={style.loginbtn} />
